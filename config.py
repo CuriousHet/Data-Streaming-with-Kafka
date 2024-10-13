b115ce41f6b7e82a152232b526b76ef734a56e32
CuriousHet <het18211@gmail.com>
2024-10-13 09:04:14 +0530

    project files

---
+++ config.py
@@ -0,0 +1,16 @@
+config = {
+    "google_api_key":"...",
+    "youtube_playlist_id": "PLodYDTuHA29aa_5UPCNg_g5fyleHxTZPU",
+
+    "kafka": {
+        "bootstrap.servers": "...",
+        "security.protocol": "sasl_ssl",
+        "sasl.mechanism": "PLAIN",
+        "sasl.username": "...",
+        "sasl.password": "...",
+    },
+    "schema_registry": {
+        "url": "...",
+        "basic.auth.user.info": "username:password",
+    }
+}
